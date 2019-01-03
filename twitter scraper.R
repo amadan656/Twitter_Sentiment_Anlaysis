0a5e62db56952cd10af5d3d4ee49a8f467c855b6
Alok Madan <35575635+amadan656@users.noreply.github.com>
2019-01-02 22:39:03 -0500

    Update twitter scraper.R

--- twitter scraper.R
+++ twitter scraper.R
@@ -1,5 +1,8 @@
+###Packages###
 install.packages("twitteR")
 install.packages("ROAuth")
+
+###Lbrarys###
 library("NLP"
 library("twitteR")
 library("syuzhet")
@@ -15,59 +18,61 @@ library("wordcloud")
 library(ggplot2)
 
 #twitter authentication
-consumer_key <- 'qy84bDNH3P5uk532PWnzuXckF'
-consumer_secret <- 'wire4O1NGJJGk4w8TabJOdEikQkW6SaI3fKffHsaI3WaExb5pI'
-access_token <- '1067072972450660353-DD5phg1crOYSxDRDLFdd3EqBzBBcpL'
-access_secret <- 'cv98QhgOXMUY1Ztz3u2FwQfoif9ikOXg8TxLeXqtVnxGG'
+consumer_key <- 'INSERT YOUR CONSUMER KEY'
+consumer_secret <- 'INSERT YOUR CONSUMER SECRET KEY'
+access_token <- 'INSERT YOUR ACCESS_TOKEN'
+access_secret <- 'INSERT YOUR ACCESS SECRET TOKEN'
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 
 ##### SEARCH QUERY #############
-tweets_s <- searchTwitter("#syfy", n=1000,lang = "en")
+tweet <- searchTwitter("YOUR SEARCH QUERY", n=1000,lang = "en")
 
 
 #convert to dataframe
-syfy_tweets <- twListToDF(tweets_s)
+tweets <- twListToDF(tweet)
 
 
 #text Only
-syfy_text<- syfy_tweets$text
+Tweet_text<- tweets$text
 
 ########clean text###############
 #convert to lower case
-syfy_text<- tolower(syfy_text)
+Tweet_text<- tolower(Tweet_text)
 # replace blank space & "rt"
-syfy_text <- gsub("rt", "", syfy_text)
+Tweet_text<- gsub("rt", "", Tweet_text)
 # Replace @UserName
-syfy_text <- gsub("@\\w+", "", syfy_text)
+Tweet_text <- gsub("@\\w+", "", Tweet_text)
 # Remove punctuation
-syfy_text <- gsub("[[:punct:]]", "", syfy_text)
+Tweet_text <- gsub("[[:punct:]]", "", Tweet_text)
 # Remove links
-syfy_text <- gsub("http\\w+", "", syfy_text)
+Tweet_text <- gsub("http\\w+", "", Tweet_text)
 # Remove tabs
-syfy_text <- gsub("[ |\t]{2,}", "", syfy_text)
+Tweet_text <- gsub("[ |\t]{2,}", "", Tweet_text)
 # Remove blank spaces at the beginning
-syfy_text <- gsub("^ ", "", syfy_text)
+Tweet_text <- gsub("^ ", "", Tweet_text)
 # Remove blank spaces at the end
-syfy_text <- gsub(" $", "", syfy_text)
+Tweet_text <- gsub(" $", "", Tweet_text)
 
-mysentiment_syfy<-get_nrc_sentiment((syfy_text))
+### SENTIMENT ANALYSIS ###
+        
+mysentiment_Tweet<-get_nrc_sentiment((Tweet_text))
 
-Sentimentscores_syfy<-data.frame(colSums(mysentiment_syfy[,]))
+Sentimentscores_Tweet<-data.frame(colSums(mysentiment_Tweet[,]))
 
-names(Sentimentscores_syfy)<-"Score"
-Sentimentscores_syfy<-cbind("sentiment"=rownames(Sentimentscores_syfy),Sentimentscores_syfy)
-rownames(Sentimentscores_syfy)<-NULL
+names(Sentimentscores_Tweet)<-"Score"
+Sentimentscores_Tweet<-cbind("sentiment"=rownames(Sentimentscores_Tweet),Sentimentscores_Tweet)
+rownames(Sentimentscores_Tweet)<-NULL
 
-ggplot(data=Sentimentscores_syfy,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sentiment),stat = "identity")+
+ggplot(data=Sentimentscores_Tweet,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sentiment),stat = "identity")+
   theme(legend.position="none")+
-  xlab("Sentiments")+ylab("scores")+ggtitle("Sentiments of people behind the tweets on Syfy")
+  xlab("Sentiments")+ylab("scores")+ggtitle("Sentiments of people behind tweets")
 
 
-syfy_text$mysentiment_syfy
+Tweet_text$mysentiment_tweets
 
 
 # Wordcloud generator
-wordcloud(syfy_text,min.freq = 10,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 100)
+wordcloud(Tweet_text,min.freq = 10,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 100)
 
